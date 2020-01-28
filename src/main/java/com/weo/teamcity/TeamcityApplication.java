0feb66da6ea8918850563c0fcfb6a04c53034403
“weodf” <weow820@gmail.com>
2020-01-28 17:42:19 +0800

    teamcity build package

---
+++ src/main/java/com/weo/teamcity/TeamcityApplication.java
@@ -0,0 +1,13 @@
+package com.weo.teamcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityApplication.class, args);
+    }
+
+}
